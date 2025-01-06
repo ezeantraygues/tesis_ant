babb9d82d1ecaefc4bac6f5cbf92a7c7d5766cd2
Juan Marí <93499906+juannpmari@users.noreply.github.com>
2025-01-06 19:57:21 -0300

    Add gdrive downloader

--- src/common/gdrive_manager.py
+++ src/common/gdrive_manager.py
@@ -90,6 +90,125 @@ class DriveAPI:
         except Exception as e:
             print(f"Error downloading file: {e}")
 
+class Downloader(DriveAPI):
+    def __init__(self, vervose=False):
+        super(Downloader, self).__init__()
+        self.DEFAULT_STORAGE_PATH = "drive_content"
+        self.vervose = vervose
+
+    def __create_Directory(self, path=None):
+        if path is None:
+            path = self.DEFAULT_STORAGE_PATH
+        if not os.path.isdir(path):
+            os.mkdir(path=path)
+
+        return path
+
+    def getAllFiles(self, folderId, destinationFolder=None):
+        path = self.__create_Directory(destinationFolder)
+        page_token = None
+
+        all_files = []
+
+        while True:
+            results = (
+                self.service.files()
+                .list(
+                    spaces="drive",
+                    pageToken=page_token,
+                    q="'{0}' in parents".format(folderId),
+                    fields="nextPageToken, files(id, name, mimeType)",
+                )
+                .execute()
+            )
+
+            items = results.get("files", [])
+            for item in items:
+                itemName = item["name"]
+                itemId = item["id"]
+                itemType = item["mimeType"]
+                if self.vervose:
+                    print("Found item: {0} with Type: {1}".format(itemName, itemType))
+                filePath = os.path.join(path, itemName)
+
+                if itemType == "application/vnd.google-apps.folder":
+                    if self.vervose:
+                        print("\nStepping into folder: {0}".format(filePath))
+                    all_files.extend(
+                        self.getAllFiles(itemId, filePath)
+                    )  # Recursive call
+                else:
+                    print("Adding file: {0}".format(filePath))
+                    all_files.append(
+                        {"item_id": itemId, "file_path": filePath, "mimeType": itemType}
+                    )
+
+            page_token = results.get("nextPageToken", None)
+            if page_token is None:
+                break
+
+        return all_files
+
+    def downloadFolder(self, folderId, destinationFolder=None):
+        all_files = self.getAllFiles(folderId, destinationFolder)
+
+        download_processes = []
+        for f in all_files:
+            p = Process(
+                target=self.downloadFile,
+                args=[f["item_id"], f["file_path"], f["mimeType"]],
+            )
+            p.start()
+            download_processes.append(p)
+
+        for process in download_processes:
+            process.join()
+
+        print("All files downloaded!")
+
+    def downloadFile(self, fileId, filePath, mimeType):
+        # Note: The parent folders in filePath must exist
+        print("\t -> Downloading file with id: {0} name: {1}".format(fileId, filePath))
+
+        # Define exportable Google Docs MIME types
+        exportable_mime_types = {
+            "application/vnd.google-apps.document": "application/pdf",
+            "application/vnd.google-apps.spreadsheet": "text/csv",
+            "application/vnd.google-apps.presentation": "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+        }
+
+        if mimeType in exportable_mime_types:
+            request = self.service.files().export_media(
+                fileId=fileId, mimeType=exportable_mime_types[mimeType]
+            )
+            file_extension = exportable_mime_types[mimeType].split("/")[-1]
+            filePath = filePath + f".{file_extension}"
+        else:
+            request = self.service.files().get_media(fileId=fileId)
+
+        fh = io.FileIO(filePath, mode="wb")
+
+        try:
+            downloader = MediaIoBaseDownload(fh, request, chunksize=1024 * 1024)
+            current_progress = -1
+            done = False
+            while not done:
+                status, done = downloader.next_chunk(num_retries=2)
+                if status:
+                    progress = int(status.progress() * 100)
+                    if progress % 10 == 0 and progress != current_progress:
+                        print(
+                            " \t Downloading fileId: {0}, {1}% complete!".format(
+                                fileId, int(status.progress() * 100)
+                            )
+                        )
+                        current_progress = progress
+            print("Download Complete!")
+        except Exception as e:
+            print(str(e))
+            return
+        finally:
+            fh.close()
 
 class Uploader(DriveAPI):
     def __init__(self):
